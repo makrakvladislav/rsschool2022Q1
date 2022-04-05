a7bd65343b9538d1dcda84a3e9344ba94c95f528
Vlad <makrak.vladislav@yandex.ru>
2022-04-06 00:54:13 +0300

    fix: fix basic structure

--- shelter/main/index.js
+++ shelter/main/index.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("111"),console.log("222");for(let o=0;o<10;o++)console.log(o)})();
+(()=>{"use strict";console.log("111"),console.log("222");for(let o=0;o<10;o++)console.log(o);new class{constructor(){console.log("Using ES2015+ syntax")}}})();
 //# sourceMappingURL=index.js.map
