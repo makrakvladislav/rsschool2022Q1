3e7c8626536ccceb84ca6d14b41e581a1c9742ad
Vlad <makrak.vladislav@yandex.ru>
2022-04-06 01:25:27 +0300

    fix: fix basic structure

---
+++ src/main/index.js
@@ -0,0 +1,19 @@
+import 'normalize.css';
+import './style.scss';
+
+console.log('111');
+
+console.log('222');
+
+
+for(let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+class TestClass {
+  constructor() {
+      let msg = "Using ES2015+ syntax";
+      console.log(msg);
+  }
+}
+let test = new TestClass();
