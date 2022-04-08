2fd6bc48645070f8771a48b7bfa3ca5e17195691
Vlad <makrak.vladislav@yandex.ru>
2022-04-09 00:16:32 +0300

    feat: add selfcheck

--- src/main/index.js
+++ src/main/index.js
@@ -1,2 +1,4 @@
 import 'normalize.css';
 import './style.scss';
+
+console.log('Main page, Score: 60');
--- src/pets/pets.js
+++ src/pets/pets.js
@@ -1,2 +1,4 @@
 import 'normalize.css';
 import './style.scss';
+
+console.log('Our Pets, Score: 60');
