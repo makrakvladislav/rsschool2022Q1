400b9bf61f62c50876d7fdd860a3f755fe01b9e3
Vlad <makrak.vladislav@yandex.ru>
2022-04-17 02:12:57 +0300

    feat: add burger menu

--- src/pets/pets.js
+++ src/pets/pets.js
@@ -1,4 +1,23 @@
 import 'normalize.css';
 import './style.scss';
 
+const body = document.body;
+const hamburger = document.querySelector(".header__burger");
+const menuWrapper = document.querySelector(".header__menu");
+const nav = document.querySelector(".nav__list");
+
+function toggleMenu() {
+  menuWrapper.classList.toggle("open");
+  body.classList.toggle("menu-open");
+}
+hamburger.addEventListener("click", toggleMenu);
+
+function closeMenu(event) {
+  if (event.target.classList.contains("nav__link")) {
+    menuWrapper.classList.remove("open");
+    body.classList.remove("menu-open");
+  }
+}
+nav.addEventListener("click", closeMenu);
+
 console.log('Our Pets, Score: 60');
