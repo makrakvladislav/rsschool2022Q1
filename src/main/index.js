40fee20f8e8fcf7ae23a4eeb8c34aa8a38a245b0
Vlad <makrak.vladislav@yandex.ru>
2022-04-22 03:27:12 +0300

    feat: add overlay for mobile menu

--- src/main/index.js
+++ src/main/index.js
@@ -4,20 +4,22 @@ import "./style.scss";
 const body = document.body;
 const hamburger = document.querySelector(".header__burger");
 const menuWrapper = document.querySelector(".header-menu__wrapper");
+const overlay = document.querySelector(".overlay");
 const nav = document.querySelector(".nav__list");
 
 function toggleMenu() {
   menuWrapper.classList.toggle("open");
   body.classList.toggle("menu-open");
+  
 }
-hamburger.addEventListener("click", toggleMenu);
 
 function closeMenu(event) {
-  if (event.target.classList.contains("nav__link")) {
-    menuWrapper.classList.remove("open");
-    body.classList.remove("menu-open");
-  }
+  menuWrapper.classList.remove("open");
+  body.classList.remove("menu-open");
 }
+
+hamburger.addEventListener("click", toggleMenu);
 nav.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
 
 console.log("Main page, Score: 60");
--- src/pets/pets.js
+++ src/pets/pets.js
@@ -1,23 +1,5 @@
 import 'normalize.css';
 import './style.scss';
-
-const body = document.body;
-const hamburger = document.querySelector(".header__burger");
-const menuWrapper = document.querySelector(".header-menu__wrapper");
-const nav = document.querySelector(".nav__list");
-
-function toggleMenu() {
-  menuWrapper.classList.toggle("open");
-  body.classList.toggle("menu-open");
-}
-hamburger.addEventListener("click", toggleMenu);
-
-function closeMenu(event) {
-  if (event.target.classList.contains("nav__link")) {
-    menuWrapper.classList.remove("open");
-    body.classList.remove("menu-open");
-  }
-}
-nav.addEventListener("click", closeMenu);
+import "../main/index";
 
 console.log('Our Pets, Score: 60');
