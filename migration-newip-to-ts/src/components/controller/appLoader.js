1aaff7c1a5ef8e05522f26141a97b37cce22bd80
Vlad <makrak.vladislav@yandex.ru>
2022-06-30 23:47:29 +0300

    feat: add apiKey

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '6d6f29398b8f4a67ae55c26af0a77e2f', // получите свой ключ https://newsapi.org/
         });
     }
 }
