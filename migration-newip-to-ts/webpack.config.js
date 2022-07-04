66623c89544b251fe239aec6c09a942b3c642dc3
Vlad <makrak.vladislav@yandex.ru>
2022-07-04 15:44:11 +0300

    fix: fix wepback.config resolve extensions

--- migration-newip-to-ts/webpack.config.js
+++ migration-newip-to-ts/webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
@@ -17,7 +17,7 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js', '.ts'],
+        extensions: ['.ts'],
     },
     output: {
         filename: 'index.js',
