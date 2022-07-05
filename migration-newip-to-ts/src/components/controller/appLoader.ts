7715644d300fc2ea7f01a921b5c98fe71ebde580
Vlad <makrak.vladislav@yandex.ru>
2022-07-06 01:36:10 +0300

    fix: change api link

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '6d6f29398b8f4a67ae55c26af0a77e2f', // получите свой ключ https://newsapi.org/
         });
     }
