a72f4998c282c5924b55e6a5a90d10d63410ab0a
Vlad <makrak.vladislav@yandex.ru>
2022-07-12 20:04:59 +0300

    feat: add control

---
+++ src/common/control.ts
@@ -0,0 +1,22 @@
+class Control<NodeType extends HTMLElement = HTMLElement> {
+  public node: NodeType;
+
+  constructor(parentNode: HTMLElement | null, tagName = "div", className = "", content = "", src = "") {
+    const el = document.createElement(tagName) as HTMLImageElement;
+    el.className = className;
+    el.innerHTML = content;
+    if (parentNode) {
+      parentNode.append(el);
+    }
+    if (tagName === "img") {
+      el.src = src;
+    }
+    this.node = el as unknown as NodeType;
+  }
+
+  destroy(): void {
+    this.node.remove();
+  }
+}
+
+export default Control;
