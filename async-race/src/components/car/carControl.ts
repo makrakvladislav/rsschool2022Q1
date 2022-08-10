fc62287ff4f84db7e91159b310f784f5a5617583
Vlad <makrak.vladislav@yandex.ru>
2022-08-10 23:24:29 +0300

    feat: implement method remove car from winners table

--- async-race/src/components/car/carControl.ts
+++ async-race/src/components/car/carControl.ts
@@ -1,5 +1,9 @@
 import Control from '../../common/control';
 import { CarsDataModel } from '../carsDataModel';
+import { WinnersView } from '../winners/winnersView';
+import { GarageView } from '../garage/garageView';
+import { PaginationView } from '../pagination/paginationView';
+import style from './car.css';
 
 export class CarControl {
   static async start(id: number) {
@@ -13,13 +17,14 @@ export class CarControl {
     const url = `http://localhost:3000/engine?id=${id}&status=drive`;
     const method = 'PATCH';
     const res = await CarsDataModel.getData(url, method);
+    // const res = await fetch(url, { method });
     return res;
   }
 
   static async carReset(id: number, parentNode: HTMLElement) {
     const url = `http://localhost:3000/engine?id=${id}&status=stopped`;
     const method = 'PATCH';
-    const res = await CarsDataModel.getData(url, method);
+    const res = await fetch(url, { method });
     parentNode.classList.remove('race', 'car-broken', 'check');
     return res;
   }
@@ -28,13 +33,34 @@ export class CarControl {
     const url = `http://localhost:3000/garage/${id}`;
     const method = 'DELETE';
     const res = await CarsDataModel.getData(url, method);
-    parentNode.destroy();
+    const carRemoveFromWinners = CarControl.carRemoveFromWinners(id, parentNode.node);
+    const carsData = new CarsDataModel([], 'winners');
+    const currentPage = localStorage.getItem('currentPage');
+    carsData.build(+currentPage!).then(async (result) => {
+      const garageWrap: HTMLElement | null = document.querySelector('.garage');
+      garageWrap!.innerHTML = '';
+      const response = await result.items!.items;
+      const itemsCount = await result.items!.itemsCount;
+      const updateGarage = new GarageView(garageWrap!, response, itemsCount!);
+      updateGarage.getCars(response);
+      const mainContainer: HTMLElement | null = document.querySelector('.main');
+      // const updatePagination = new PaginationView(itemsCount!, mainContainer!);
+      PaginationView.update(itemsCount!, mainContainer!);
+      console.log(result.data);
+    });
+    // parentNode.destroy();
+  }
+
+  static async carRemoveFromWinners(id: number, parentNode: HTMLElement) {
+    const url = `http://localhost:3000/winners/${id}`;
+    const method = 'DELETE';
+    const res = await CarsDataModel.getData(url, method);
+    const winnersView = new WinnersView('time', 'ASC');
   }
 
   static broken(parentNode: HTMLElement) {
     console.log('car broken');
     parentNode.classList.add('car-broken');
-    parentNode.classList.add('check');
   }
 }
 
